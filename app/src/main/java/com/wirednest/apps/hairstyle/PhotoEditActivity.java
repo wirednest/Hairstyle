ae1ae2030dcb1aca2b192e8500e744046aa50566
Jojo <jojo@wirednest.com>
2015-11-18 09:58:16 +0700

    Edit photo : resize, rotate , dragging

--- app/src/main/java/com/wirednest/apps/hairstyle/PhotoEditActivity.java
+++ app/src/main/java/com/wirednest/apps/hairstyle/PhotoEditActivity.java
@@ -2,14 +2,35 @@ package com.wirednest.apps.hairstyle;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
-public class PhotoEditActivity extends Activity {
+import com.squareup.picasso.Picasso;
+
+public class PhotoEditActivity extends Activity implements View.OnTouchListener {
+    // these matrices will be used to move and zoom image
+    private Matrix matrix = new Matrix();
+    private Matrix savedMatrix = new Matrix();
+    // we can be in one of these 3 states
+    private static final int NONE = 0;
+    private static final int DRAG = 1;
+    private static final int ZOOM = 2;
+    private int mode = NONE;
+    // remember some things for zooming
+    private PointF start = new PointF();
+    private PointF mid = new PointF();
+    private float oldDist = 1f;
+    private float d = 0f;
+    private float newRot = 0f;
+    private float[] lastEvent = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +46,20 @@ public class PhotoEditActivity extends Activity {
                 startActivityForResult(intent, 14045);
             }
         });
-
+        ImageView view = (ImageView) findViewById(R.id.hair);
+        view.setOnTouchListener(this);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 14045 && resultCode == RESULT_OK){
-            int idGambiran = data.getIntExtra("ID_BUKOT",-1);
-            if(idGambiran != -1) {
+        if (requestCode == 14045 && resultCode == RESULT_OK) {
+            int idGambiran = data.getIntExtra("ID_BUKOT", -1);
+
+            if (idGambiran != -1) {
                 ImageView imagine = (ImageView) findViewById(R.id.hair);
-                imagine.setImageResource(idGambiran);
+                Picasso.with(this).load(idGambiran).into(imagine);
+//                imagine.setImageDrawable(getResources().getDrawable(idGambiran));
             }
         }
     }
@@ -60,5 +84,99 @@ public class PhotoEditActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onTouch(View v, MotionEvent event) {
+        // handle touch events here
+        ImageView view = (ImageView) v;
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                mode = DRAG;
+                lastEvent = null;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                oldDist = spacing(event);
+                if (oldDist > 10f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+                lastEvent = new float[4];
+                lastEvent[0] = event.getX(0);
+                lastEvent[1] = event.getX(1);
+                lastEvent[2] = event.getY(0);
+                lastEvent[3] = event.getY(1);
+                d = rotation(event);
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = NONE;
+                lastEvent = null;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mode == DRAG) {
+                    matrix.set(savedMatrix);
+                    float dx = event.getX() - start.x;
+                    float dy = event.getY() - start.y;
+                    matrix.postTranslate(dx, dy);
+                } else if (mode == ZOOM) {
+                    float newDist = spacing(event);
+                    if (newDist > 10f) {
+                        matrix.set(savedMatrix);
+                        float scale = (newDist / oldDist);
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                    //pointer count means finger
+                    if (lastEvent != null && event.getPointerCount() == 2) {
+                        newRot = rotation(event);
+                        float r = newRot - d;
+                        float[] values = new float[9];
+                        matrix.getValues(values);
+                        float tx = values[2];
+                        float ty = values[5];
+                        float sx = values[0];
+                        float xc = (view.getWidth() / 2) * sx;
+                        float yc = (view.getHeight() / 2) * sx;
+                        matrix.postRotate(r, tx + xc, ty + yc);
+                    }
+                }
+                break;
+        }
+
+        view.setImageMatrix(matrix);
+        return true;
+    }
+
+    /**
+     * Determine the space between the first two fingers
+     */
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    /**
+     * Calculate the mid point of the first two fingers
+     */
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
+    /**
+     * Calculate the degree to be rotated by.
+     *
+     * @param event
+     * @return Degrees
+     */
+    private float rotation(MotionEvent event) {
+        double delta_x = (event.getX(0) - event.getX(1));
+        double delta_y = (event.getY(0) - event.getY(1));
+        double radians = Math.atan2(delta_y, delta_x);
+        return (float) Math.toDegrees(radians);
     }
 }
