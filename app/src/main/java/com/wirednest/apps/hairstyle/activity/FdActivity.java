fbc2f1ca30bebb8ee683958f5de65d20cf7cf837
Jojo <jojo@wirednest.com>
2015-12-14 18:58:28 +0700

    jajal yang lain

--- app/src/main/java/com/wirednest/apps/hairstyle/activity/FdActivity.java
+++ app/src/main/java/com/wirednest/apps/hairstyle/activity/FdActivity.java
@@ -1,240 +1,283 @@
 package com.wirednest.apps.hairstyle.activity;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import android.app.Activity;
+import android.content.Context;
 import android.view.View;
-import android.view.Window;
-import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.ToggleButton;
-import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Toast;
 
-import com.wirednest.apps.hairstyle.fd.FdView;
-import com.wirednest.apps.hairstyle.fd.VerticalSeekBar;
+import com.wirednest.apps.hairstyle.R;
+import com.wirednest.apps.hairstyle.fd.DetectionBasedTracker;
 
-public class FdActivity extends Activity {
-	private static final String TAG = "Sample::Activity";
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-	private MenuItem mItemFace50;
-	private MenuItem mItemFace40;
-	private MenuItem mItemFace30;
-	private MenuItem mItemFace20;
-	private MenuItem mItemType;
+import static org.opencv.android.OpenCVLoader.*;
 
-	private FdView mView = new FdView(this);
-	private TextView matching_method;
-	public static int method = 1;
 
-	private BaseLoaderCallback mOpenCVCallBack = new BaseLoaderCallback(this) {
+public class FdActivity extends AppCompatActivity implements CvCameraViewListener2{
+
+	private static final String    TAG                     = "Main::Activity";
+	private static final Scalar    FACE_RECT_COLOR         = new Scalar(0, 255, 0, 255);
+	public static final int        JAVA_DETECTOR           = 0;
+	public static final int        NATIVE_DETECTOR         = 1;
+
+	private MenuItem               mItemType;
+
+	private Mat                    mRgba;
+	private Mat                    mGray;
+	private File mCascadeFile;
+	private CascadeClassifier      mJavaDetector;
+	private DetectionBasedTracker mNativeDetector;
+
+	private int                    mDetectorType           = JAVA_DETECTOR;
+	private String[]               mDetectorName;
+
+	private float                  mRelativeFaceSize       = 0.2f;
+	private int                    mAbsoluteFaceSize       = 0;
+
+	private CameraBridgeViewBase   mOpenCvCameraView;
+
+	//Some UI items
+	public TextView                textView                = null;
+
+    /*Note This method is deprecated for production code. It is designed for experimental and local development purposes only.
+    //If you want to publish your app use approach with async initialization.
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            //Handle initialization error
+            Log.i(TAG, "NO LOVE 4 U :: BYE!");
+        }
+        System.loadLibrary("opencv_java");
+    }*/
+
+
+
+
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 		@Override
-		public void onManagerConnected(int status) {
-			switch (status) {
-			case LoaderCallbackInterface.SUCCESS: {
-				Log.i(TAG, "OpenCV loaded successfully");
+		public void onManagerConnected(int status){
+			switch(status){
+				case LoaderCallbackInterface.SUCCESS:
+				{
+					Log.i(TAG, "OpenCV Loaded Successfully");
+					System.loadLibrary("detection_based_tracker");
 
-				// Load native libs after OpenCV initialization
-				// System.loadLibrary("detection_based_tracker");
+					try{
+						// load cascade file from application resources
+						InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+						File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+						mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+						FileOutputStream os = new FileOutputStream(mCascadeFile);
 
-				// Create and set View
-				mView = new FdView(mAppContext);
-				mView.setDetectorType(mDetectorType);
-				mView.setMinFaceSize(0.2f);
-
-				VerticalSeekBar VerticalseekBar = new VerticalSeekBar(
-						getApplicationContext());
-				VerticalseekBar.setMax(5);
-				VerticalseekBar.setPadding(20, 20, 20, 20);
-				RelativeLayout.LayoutParams vsek = new RelativeLayout.LayoutParams(
-						RelativeLayout.LayoutParams.WRAP_CONTENT, 400);
-				vsek.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-				VerticalseekBar.setId(100);
-				VerticalseekBar
-						.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-
-							public void onProgressChanged(SeekBar seekBar,
-									int progress, boolean fromUser) {
-
-								method = progress;
-								switch (method) {
-								case 0:
-									matching_method.setText("TM_SQDIFF");
-									break;
-								case 1:
-									matching_method.setText("TM_SQDIFF_NORMED");
-									break;
-								case 2:
-									matching_method.setText("TM_CCOEFF");
-									break;
-								case 3:
-									matching_method.setText("TM_CCOEFF_NORMED");
-									break;
-								case 4:
-									matching_method.setText("TM_CCORR");
-									break;
-								case 5:
-									matching_method.setText("TM_CCORR_NORMED");
-									break;
-								}
-
-							}
-
-							public void onStartTrackingTouch(SeekBar seekBar) {
-							}
-
-							public void onStopTrackingTouch(SeekBar seekBar) {
-							}
-						});
-
-				matching_method = new TextView(getApplicationContext());
-				matching_method.setText("TM_SQDIFF");
-				matching_method.setTextColor(Color.YELLOW);
-				RelativeLayout.LayoutParams matching_method_param = new RelativeLayout.LayoutParams(
-						RelativeLayout.LayoutParams.WRAP_CONTENT,
-						RelativeLayout.LayoutParams.WRAP_CONTENT);
-				matching_method_param
-						.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-				matching_method_param.addRule(RelativeLayout.BELOW,
-						VerticalseekBar.getId());
-
-				Button btn = new Button(getApplicationContext());
-				btn.setText("Create template");
-				RelativeLayout.LayoutParams btnp = new RelativeLayout.LayoutParams(
-						RelativeLayout.LayoutParams.WRAP_CONTENT,
-						RelativeLayout.LayoutParams.WRAP_CONTENT);
-				btnp.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-				btn.setId(90);
-
-				btn.setOnClickListener(new OnClickListener() {
-					public void onClick(View v) {
-						mView.resetLearFramesCount();
-					}
-				});
-
-				RelativeLayout frameLayout = new RelativeLayout(
-						getApplicationContext());
-				frameLayout.addView(mView, 0);
-				frameLayout.addView(btn, btnp);
-
-				frameLayout.addView(VerticalseekBar, vsek);
-				frameLayout.addView(matching_method, matching_method_param);
-
-				setContentView(frameLayout);
-
-				// Check native OpenCV camera
-				if (!mView.openCamera()) {
-					AlertDialog ad = new AlertDialog.Builder(mAppContext)
-							.create();
-					ad.setCancelable(false); // This blocks the 'BACK' button
-					ad.setMessage("Fatal error: can't open camera!");
-					ad.setButton("OK", new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int which) {
-							dialog.dismiss();
-							finish();
+						byte[] buffer = new byte[4096];
+						int bytesRead;
+						while ((bytesRead = is.read(buffer)) != -1) {
+							os.write(buffer, 0, bytesRead);
 						}
-					});
-					ad.show();
-				}
-			}
-				break;
-			default: {
-				super.onManagerConnected(status);
-			}
-				break;
-			}
-		}
-	};
+						is.close();
+						os.close();
 
-	private int mDetectorType = 0;
-	private String[] mDetectorName;
+						mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+						if (mJavaDetector.empty()) {
+							Log.e(TAG, "Failed to load cascade classifier");
+							mJavaDetector = null;
+						} else
+							Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
 
-	public FdActivity() {
-		Log.i(TAG, "Instantiated new " + this.getClass());
+						mNativeDetector = new DetectionBasedTracker(mCascadeFile.getAbsolutePath(), 0);
+
+						cascadeDir.delete();
+					}catch(IOException e){
+						e.printStackTrace();
+						Log.i(TAG, "Failed to load cascade. Exception thrown: " + e);
+					}
+
+					//public static final int CAMERA_ID_BACK  = 99;
+					//public static final int CAMERA_ID_FRONT = 98;
+					//mOpenCvCameraView.setCameraIndex(98);
+					//mOpenCvCameraView.setCameraIndex(1);
+					//mOpenCvCameraView.enableFpsMeter();
+					mOpenCvCameraView.enableView();
+
+				} break;
+				default:
+				{
+					super.onManagerConnected(status);
+				}break;
+			}//switch
+		}//onManagerConnected
+	};//BaseLoaderCallback
+
+	public FdActivity(){
 		mDetectorName = new String[2];
-		mDetectorName[FdView.JAVA_DETECTOR] = "Java";
-		mDetectorName[FdView.NATIVE_DETECTOR] = "Native (tracking)";
+		mDetectorName[JAVA_DETECTOR] = "JAVA";
+		mDetectorName[NATIVE_DETECTOR] = "NATIVE (tracking)";
 	}
 
+	/** Called when the activity is first created. **/
 	@Override
-	protected void onPause() {
-		Log.i(TAG, "onPause");
-		super.onPause();
-		if (mView != null)
-			mView.releaseCamera();
-	}
-
-	@Override
-	protected void onResume() {
-		Log.i(TAG, "onResume");
-		super.onResume();
-		if (mView != null && !mView.openCamera()) {
-			AlertDialog ad = new AlertDialog.Builder(this).create();
-			ad.setCancelable(false); // This blocks the 'BACK' button
-			ad.setMessage("Fatal error: can't open camera!");
-			ad.setButton("OK", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					dialog.dismiss();
-					finish();
-				}
-			});
-			ad.show();
-		}
-	}
-
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		Log.i(TAG, "onCreate");
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, "called onCreate");
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		setContentView(R.layout.face_detect_surface_view);
 
-		Log.i(TAG, "Trying to load OpenCV library");
-		if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_2, this,
-				mOpenCVCallBack)) {
-			Log.e(TAG, "Cannot connect to OpenCV Manager");
+		mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.fd_activity_surface_view);
+		mOpenCvCameraView.setCameraIndex(99);// back camera
+		mOpenCvCameraView.setRotation(90);
+		mOpenCvCameraView.setCvCameraViewListener(this);
+
+		//OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_11, this, mLoaderCallback);
+		//if(!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_11, this, mLoaderCallback))
+		//mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+	}
+
+	@Override
+	public void onPause(){
+		super.onPause();
+		if(mOpenCvCameraView != null)
+			mOpenCvCameraView.disableView();
+	}
+
+	@Override
+	public void onResume(){
+		super.onResume();
+		if(!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_11, this, mLoaderCallback)){
+			Log.i(TAG, "OpenCVLoader Failed on Resume");
+		}
+		//mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+
+	}
+
+	public void onDestroy(){
+		super.onDestroy();
+		mOpenCvCameraView.disableView();
+	}
+
+	public void onCameraViewStarted(int width, int height){
+		mGray = new Mat();
+		mRgba = new Mat();
+	}
+
+	public void onCameraViewStopped(){
+		mGray.release();
+		mRgba.release();
+	}
+
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+
+		mRgba = inputFrame.rgba();
+		mGray = inputFrame.gray();
+
+
+		//Core.transpose(mRgba, mGray);
+		//Camera Upside down Fix...
+		Core.flip(mRgba, mGray, -1);
+
+		if (mAbsoluteFaceSize == 0) {
+			int height = mGray.rows();
+			if (Math.round(height * mRelativeFaceSize) > 0) {
+				mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+			}
+			mNativeDetector.setMinFaceSize(mAbsoluteFaceSize);
+		}
+
+		MatOfRect faces = new MatOfRect();
+
+
+		if (mDetectorType == JAVA_DETECTOR) {
+			if (mJavaDetector != null)
+				mJavaDetector.detectMultiScale(mGray, faces, 1.1, 2, 2, // TODO: objdetect.CV_HAAR_SCALE_IMAGE
+						new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+		}
+		else if (mDetectorType == NATIVE_DETECTOR) {
+			if (mNativeDetector != null)
+				mNativeDetector.detect(mGray, faces);
+		}
+		else {
+			Log.e(TAG, "Detection method is not selected!");
+		}
+
+		Rect[] facesArray = faces.toArray();
+		for (int i = 0; i < facesArray.length; i++)
+			Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), FACE_RECT_COLOR, 3);
+
+		return mRgba;
+	}
+
+	private void setMinFaceSize(float faceSize){
+		mRelativeFaceSize = faceSize;
+		mAbsoluteFaceSize = 0;
+	}
+
+	private void setDetectorType(int type) {
+		if (mDetectorType != type) {
+			mDetectorType = type;
+
+			if (type == NATIVE_DETECTOR) {
+				Log.i(TAG, "Detection Based Tracker enabled");
+				mNativeDetector.start();
+			} else {
+				Log.i(TAG, "Cascade detector enabled");
+				mNativeDetector.stop();
+			}
 		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		Log.i(TAG, "onCreateOptionsMenu");
-		mItemFace50 = menu.add("Face size 50%");
-		mItemFace40 = menu.add("Face size 40%");
-		mItemFace30 = menu.add("Face size 30%");
-		mItemFace20 = menu.add("Face size 20%");
+		// Inflate the menu; this adds items to the action bar if it is present.
+		//getMenuInflater().inflate(R.menu.menu_fd, menu);
 		mItemType = menu.add(mDetectorName[mDetectorType]);
-
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		Log.i(TAG, "Menu Item selected " + item);
-		if (item == mItemFace50)
-			mView.setMinFaceSize(0.5f);
-		else if (item == mItemFace40)
-			mView.setMinFaceSize(0.4f);
-		else if (item == mItemFace30)
-			mView.setMinFaceSize(0.3f);
-		else if (item == mItemFace20)
-			mView.setMinFaceSize(0.2f);
-		else if (item == mItemType) {
-			mDetectorType = (mDetectorType + 1) % mDetectorName.length;
-			item.setTitle(mDetectorName[mDetectorType]);
-			mView.setDetectorType(mDetectorType);
-		}
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		//int id = item.getItemId();
+
+        /*noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+        */
+		int tmpDetectorType = (mDetectorType + 1) % mDetectorName.length;
+		Toast.makeText(this, mDetectorName[tmpDetectorType],Toast.LENGTH_LONG);
+		textView = (TextView) findViewById(R.id.my_textview);
+		textView.setText(mDetectorName[tmpDetectorType]);
+		setDetectorType(tmpDetectorType);
 		return true;
 	}
+
 }
